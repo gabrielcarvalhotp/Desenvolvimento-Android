6fd7ef4fdf3e9758cc6e3008fd930d043ba938ee
gabrielcarvalhotp <gabrieltp2004@gmail.com>
2024-01-30 20:34:01 -0300

    starting in java

---
+++ Seção 9 - Linguagem Java/50 O que são variáveis e constantes/untitled/src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "Gabriel Carvalho";
+        System.out.println(nome);
+
+        int number = 10;
+        int number2 = 5;
+        System.out.println(number + number2);
+
+        final double PI = 3.141516;
+        //PI = 123
+        System.out.println(PI);
+
+        double height = 1.82;
+        double weight = 75;
+        double imc = weight / (height * height);
+        System.out.println(imc);
+    }
+}
