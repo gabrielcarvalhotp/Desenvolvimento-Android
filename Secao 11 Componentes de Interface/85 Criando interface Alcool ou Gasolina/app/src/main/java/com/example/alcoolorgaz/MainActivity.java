a9a239e983ca45992d5389a5e7ec96f1b2aceb84
gabrielcarvalhotp <gabrieltp2004@gmail.com>
2024-02-11 12:54:45 -0300

    app alcohol or Gaz

---
+++ Secao 11 Componentes de Interface/85 Criando interface Alcool ou Gasolina/app/src/main/java/com/example/alcoolorgaz/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.alcoolorgaz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText edtPriceAlcohol, edtPriceGaz;
+    TextView txtResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtPriceAlcohol = findViewById(R.id.edtPriceAlcohol);
+        edtPriceGaz = findViewById(R.id.edtPriceGaz);
+        txtResult = findViewById(R.id.txtResult);
+    }
+
+    public void calculate(View view) {
+        Double priceAlcohol, priceGaz;
+        try {
+            priceAlcohol = Double.parseDouble(edtPriceAlcohol.getText().toString());
+            priceGaz = Double.parseDouble(edtPriceGaz.getText().toString());
+        } catch (NumberFormatException e) {
+            txtResult.setText("Preencha os campos com valores validos!");
+            return;
+        }
+
+        if (priceAlcohol / priceGaz < 0.7) {
+            txtResult.setText("Abasteça com alcool");
+        } else
+            txtResult.setText("Abasteça com gasolina");
+    }
+}
